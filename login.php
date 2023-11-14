24b69586577a0f552aafe3fe2e199d0354787f8e
SouidIskandar <souidiskandar32@gmail.com>
2023-11-14 22:44:33 +0100

    initial commit

---
+++ login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <title>Dary</title>
+</head>
+
+    <header>
+        <div class="nav container">
+            <a href="index.php" class="logo"><i class='bx bx-home-heart'></i>Dary</a>
+            <a href="Sign-up.php" class="btn">Sign Up</a>
+        </div>
+    </header>
+    <div class="login container">
+      <div class="login-container">
+         <h2>Login To Continue</h2>
+         <p>Log in with your data that you entered <br> during your registration</p>
+         <form action="">
+            <span>Enter your E-mail adress</span>
+            <input type="email" name="" id="" placeholder="yourmail@gmail.com" required>
+            <span>Enter your password</span>
+            <input type="password" name="" id="" placeholder="password" required>
+            <input type="submit" value="Log In" class="buttom">
+            <a href="#">Forget Password ?</a>
+         </form>
+         <a href="Sign-up.php" class="btn">Sign up now</a>
+      </div>
+      <div class="login-image">
+         <img src="4207.jpg" alt="" width="848px>
+      </div>
+    </div>
+
+
+<section class="footer">
+        <div class="footer-container container">
+            <h2>Dary</h2>
+            <div class="footer-box">
+                <h3>Quick Links</h3>
+                <a href="#">Agency</a>
+                <a href="#">Building</a>
+                <a href="#">Rates</a>
+            </div>
+            <div class="footer-box">
+                <h3>Locations</h3>
+                <a href="#">Tunis</a>
+                <a href="#">Sousse</a>
+                <a href="#">Monastir</a>
+            </div>
+            <div class="footer-box">
+                <h3>Contact</h3>
+                <a href="#">(+216) 50 528 088</a>
+                <a href="#">Souidiskandar32@gmail.com</a>
+                <div class="social">
+                    <a href="#"><i class='bx bxl-facebook'></i></a>
+                    <a href="#"><i class='bx bxl-instagram'></i></a>
+                    <a href="#"><i class='bx bxl-whatsapp' ></i></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="copyright">
+        <p>&#169; SouidIskandar All right Reserved</p>
+    </div>
+
+   
+    
+      
+</body>
+</html>
